9ac8775b28e2cd05e84ab81fb0e9f3a6e6f9a6ef
unknown <agudelomariana2503@gmail.com>
2023-02-11 19:19:43 -0500

    Funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,71 @@
-import validator from './validator.js';
+// //import validator from './validator.js';
+// //console.log(validator);
 
-console.log(validator);
+// const enviar = document.getElementById("enviar");
+// enviar.addEventListener("click", function(event) {
+//   event.preventDefault();
+//   //Lista con el número de la tarjeta
+//   const lista = document.getElementsByClassName("num");
+  
+//   const numero = [];
+//   for (const elemento of lista) {
+//     numero.push(elemento.value);
+//   }
+//   //Reversamos la lista
+//   numero.reverse(); 
+//   //Multiplicamos las posiciones pares por dos
+//   let i=1;
+//   while(i <=(numero.length-1)){
+//     numero[i]=numero[i]*2;
+//     i=i+2;
+//   }
+//   let k=0;
+//   for (numero[k] of numero){
+//     if (numero[k]>9){
+//       let sep=[];
+//       sep = numero[k].toString().split('').map(digit => parseInt(digit));
+//       numero[k]=sep[0]+sep[1];      
+//     }
+//     k++;
+//   }
+//   const total = numero.map(string => parseInt(string));
+//   const suma = total.reduce((total, current) => total + current);
+//   if (suma%10===0){
+//     alert('Es válida');
+//   }
+// });
+import validator from './validator.js';
+//Llamamos el número de tarjeta solicitado
+const enviar = document.getElementById("enviar");
+enviar.addEventListener("click", function(event) {
+  event.preventDefault();
+  //Lista con el número de la tarjeta
+  const lista = document.getElementsByClassName("num");
+  //Si el valor ingresado no es vacío, hacer esto:
+  if (lista[0].value!==''){
+    lista[0].toString().split('').map(digit => parseInt(digit));
+    const numero = [];
+    for (const elemento of lista) {
+      numero.push(elemento.value);
+    }
+    //Validación de la tarjeta de crédito
+    const creditCardNumber = numero.join('');
+    if (validator.isValid(creditCardNumber)) {
+      alert('Es válida');
+    } else {
+      alert('No es válida');
+    }
+    //Ocultar número de la tarjeta
+    let maskedNumber =0;
+    if (creditCardNumber.length>4){
+      maskedNumber = validator.maskify(creditCardNumber);
+    } else{
+      maskedNumber=creditCardNumber;
+    }
+    //No se puede ingresadr un espacio vacío
+    console.log(validator.checkFranchise(creditCardNumber));
+  } else{
+    alert('Ingresa un valor para continuar')
+  }
+});
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,113 @@
-const validator = {
-  // ...
-};
+function isValid(creditCardNumber) {
+  const numero = [];
+  for (let i = 0; i < creditCardNumber.length; i++) {
+    numero.push(parseInt(creditCardNumber[i]));
+  }
+  numero.reverse(); 
+  let i = 1;
+  while (i <= (numero.length - 1)) {
+    numero[i] = numero[i] * 2;
+    i = i + 2;
+  }
+  let k = 0;
+  for (numero[k] of numero) {
+    if (numero[k] > 9) {
+      let sep = [];
+      sep = numero[k].toString().split('').map(digit => parseInt(digit));
+      numero[k] = sep[0] + sep[1];      
+    }
+    k++;
+  }
+  const total = numero.map(string => parseInt(string));
+  const suma = total.reduce((total, current) => total + current);
+  return suma % 10 === 0;
+}
 
-export default validator;
+function maskify(creditCardNumber) {
+  const lastFour = creditCardNumber.slice(-4);
+  const masked = '*'.repeat(creditCardNumber.length - 4);
+  return masked + lastFour;
+}
+// 51-55 --- mc dígitos 16
+// 4 --- visa dígitos 13 o 16
+// 34, 37 --- amex dígitos 15
+// 300-305 --- diners carte blanche dígitos 14
+// 36 --- diners int dígitos 14
+// 6011 --- discover dígitos 16
+// 2014, 2149 diners er --- dígitos 14
+// 3 --- jcb dígitos 16
+// 2131, 1800 --- jcb dígitos 16
+function checkFranchise(creditCardNumber){
+  let banco='';
+
+  //Banco jcb
+  if (creditCardNumber.slice(0, 4) === '1800' && creditCardNumber.length === 16 || creditCardNumber.slice(0, 4) === '2131' && creditCardNumber.length === 16) {
+    banco='JCB'    
+  }
+  //Banco jcb
+  else if (creditCardNumber.charAt(0)==='3' && creditCardNumber.length === 16) 
+  {
+    banco='JCB';    
+  }
+
+  //Banco Dinner's Club/ enRoute
+  else if (creditCardNumber.slice(0, 4) === '2014' && creditCardNumber.length === 14 || 
+      creditCardNumber.slice(0, 4) === '2149' && creditCardNumber.length === 14)
+  {    
+    banco="Dinner's Club/ enRoute";    
+  }
+
+  //Banco Dinner's Club/ Carte Blanche
+  else if (creditCardNumber.length === 14 && Number(creditCardNumber.slice(0, 3)) >= 300 && Number(creditCardNumber.slice(0, 3)) <= 305)
+  {    
+    banco="Dinner's Club/ Carte Blanche";    
+  }
+
+  //Banco Dinner's Club/ Carte Blanche
+  else if (creditCardNumber.slice(0, 2) === '38' && creditCardNumber.length === 14)
+  {    
+    banco="Dinner's Club/ Carte Blanche";    
+  }
+
+  //Banco Dinner's Club/ International
+  else if (creditCardNumber.slice(0, 2) === '36' && creditCardNumber.length === 14)
+  {    
+    banco="Banco Dinner's Club/ International";    
+  }
+
+  //Banco American Express
+  else if (creditCardNumber.slice(0, 2) === '34' && creditCardNumber.length === 15 || creditCardNumber.slice(0, 2) === '37' && creditCardNumber.length === 15)
+  {    
+    banco="American Express";    
+  }
+
+  //Banco Visa
+  else if ((creditCardNumber.slice(0, 1) === '4' && creditCardNumber.length === 16)||
+           (creditCardNumber.slice(0, 1) === '4' && creditCardNumber.length === 13))
+  {    
+    banco="Visa";    
+  }
+
+  //Banco MasterCard
+  else if (creditCardNumber.length === 16 && creditCardNumber.slice(0, 2) >= 51 && creditCardNumber.slice(0, 2) <= 55) {
+    banco = "MasterCard";
+  }
+
+  //Banco Discover
+  else if (creditCardNumber.slice(0, 4) === '6011' && creditCardNumber.length === 16)
+  {    
+    banco="Discover";    
+  }
+
+  else{
+    banco='No identificado';
+  }
+
+  return(alert(banco));
+}
+
+export default {
+  isValid: isValid,
+  maskify: maskify,
+  checkFranchise: checkFranchise,
+};
